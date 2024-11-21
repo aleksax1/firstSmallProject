cc015ed09c0d287e170434be6fa781e304df1df4
alekax1 <aleksajankovic195@gmial.com>
2024-11-21 17:05:40 +0100

    btn to up update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.scss';
+import { IoIosArrowDropupCircle } from "react-icons/io";
 import NewsCard from './components/newCard/NewsCard.jsx';
 import Nav from './components/nav/Nav.jsx';
 import Footer from './components/footer/Footer.jsx';
@@ -14,7 +15,7 @@ function App() {
         const response = await 
   
         axios.get(
-          'https://newsapi.org/v2/everything?q=tesla&from=2024-11-1&sortBy=publishedAt&pageSize=12&apiKey=478f6f782df94f85ae8504f796503aad'
+          'https://newsapi.org/v2/everything?q=tesla&from=2024-11-1&sortBy=publishedAt&pageSize=15&apiKey=478f6f782df94f85ae8504f796503aad'
         );
         setArticles(response.data.articles);
         console.log(response.data.articles);
@@ -38,6 +39,27 @@ function App() {
         ))}
       </div>
     </div>
+    <div className='arrowUp'
+        style={{
+          position: "fixed",
+          bottom: 40,
+          right: 12,
+          height: 60,
+          width: "10vw",
+          backgroundColor: "gray",
+          cursor: "pointer",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius:"40px"
+        }}
+        onClick={() => window.scroll({ top: 0, behavior: "smooth" })}>
+        <h5 
+        style={{
+          paddingRight:"5px"
+        }}>GO TO TOP</h5>
+         <IoIosArrowDropupCircle size={35} />
+      </div>
         <Footer/>
     </>
   );
